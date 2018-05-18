d2de9fa6b70f4be61beae6dbab6a1664e04d2918
Thao Truong <phuongthao97py@gmail.com>
2018-05-18 15:49:37 -0700

    first commit

---
+++ game_server.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request, redirect, session
+import random
+app=Flask(__name__)
+app.secret_key = "ThisIsSecured"
+@app.route('/')
+def game_interface():
+    if'out' not in session:
+        session['out']=''
+    return render_template('game.html', output=session['out'])
+
+@app.route('/process', methods=['POST'])
+def compare():
+    num=50
+    print num
+    guess=request.form['numbers']
+    print guess
+    if'out' not in session:
+        session['out']=''
+    
+    if num<int(guess):
+        result='Too High'
+    elif num>int(guess):
+        result = 'Too Low'
+    else:
+        result=num
+    
+    session['out']=result
+    return redirect('/')
+
+@app.route('/clear')
+def session_clear():
+    session.clear()
+    return redirect('/')
+
+@app.route('/restart')
+def restart():
+    session.pop('out')
+    return redirect('/')
+
+app.run(debug=True)
